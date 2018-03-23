d3f867f2a6248cfbf955f850a18facbfedbc9605
贺鑫鑫 <hexinxin@quyiyuan.com>
2018-03-23 19:06:55 +0800

    描述：将config server 注册到注册中心 作者：mervyn

--- src/main/java/com/mervyn/ConfigServerApplication.java
+++ src/main/java/com/mervyn/ConfigServerApplication.java
@@ -3,8 +3,9 @@ package com.mervyn;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
